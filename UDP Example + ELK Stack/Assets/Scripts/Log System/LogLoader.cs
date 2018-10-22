eb2a9553d431efade4ff52d01b880915ead4e04b
ChipuChipu <Joshuay.yi@hotmail.com>
2018-10-22 02:21:41 -0700

    Added Log System

---
+++ UDP Example + ELK Stack/Assets/Scripts/Log System/LogLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;    // For StreamWriter
+
+public static class LogLoader
+{
+    public static void Log(string filePath, string log)
+    {
+        using (StreamWriter sw = new StreamWriter(filePath))
+        {
+            sw.WriteLine(log);
+            sw.Close();
+        }
+    }
+}
---
+++ UDP Example + ELK Stack/Assets/Scripts/Log System/LogStructure.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/*
+    THINGS-TO-DO:
+    (1) Flesh out LogLoader with a way to save a list as a file
+    (2) Provide Checks to see when a valid connect exists, we send a log.
+    (3) Provide additional checks when a valid connect exists, if there are any logs saved on local. We fetch, send, and delete.
+*/
+
+public class LogStructure : Singleton<LogStructure>
+{
+    #region Singleton Initialization
+    [RuntimeInitializeOnLoadMethodAttribute(RuntimeInitializeLoadType.AfterSceneLoad)]
+    public static void InitializeStructure()
+    {
+        InitializeSingleton();
+    }
+    #endregion
+
+    #region Initialization
+    public static string filePath = "C:\\Users\\Joshu\\Documents\\GitHub\\ELKStack-Prototype\\Log Dump\\" + "ErrorLog -- " + DateTime.UtcNow.Date + ".txt";
+
+    private List<string> _LogList;
+    static List<string> LogList
+    {
+        get { return Instance._LogList; }
+        set
+        {
+            Instance._LogList = value;
+        }
+    }
+
+    private void Awake()
+    {
+        _LogList = new List<string>();
+
+        // Should a check be added here to see if the log dump has an files inside?
+
+    }
+    #endregion
+
+    #region Core Methods
+    public static void Log(string log)
+    {
+        LogList.Add(log);
+        LogLoader.Log(filePath, log);
+
+        // Should see if We have Connection. If so, send away. Else, stash it.
+        // Should also see if the log dump has entities. If so, fetch and send.
+        // Empty all files out of the Log Dump after sending.
+    }
+    #endregion
+
+    #region Stop()
+    private void Stop()
+    {
+
+    }
+    #endregion
+
+    #region OnApplicationQuit()
+    private void OnApplicationQuit()
+    {
+        
+    }
+    #endregion
+}
